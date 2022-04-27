09c4d48fa8e58843a916e72e6bedadcd1557ed54
Javier.Pesado <javier.pesado@insurancerevolution.co.uk>
2022-04-27 11:32:48 +0100

    login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,7 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');//-> route name (alias)
 Route::post('/register', [RegisterController::class, 'store']);
+
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');//-> route name (alias)
+Route::post('/login', [LoginController::class, 'store']);
