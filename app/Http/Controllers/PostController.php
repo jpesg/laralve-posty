45ad9e3be253fdf7f3a100c2306e84ba9410b49c
Javier.Pesado <javier.pesado@insurancerevolution.co.uk>
2022-04-27 16:04:14 +0100

    create posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+//php artisan make:model Post -m -f
+class PostController extends Controller
+{
+    public function index()
+    {
+        $post = Post::get();//all post --> Laravel Collection
+
+        return view('posts.index', [
+            'posts' => $post
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        /*Post::create([
+            'user_id' => auth()->id(),
+            'body' => $request->body
+        ]);*/
+
+        /*$request->user()->posts()->create([
+            'body' => $request->body
+        ]);*/
+
+        $request->user()->posts()->create($request->only('body'));
+
+        return redirect()->back();
+
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/', function(){
@@ -34,3 +33,6 @@ Route::get('/login', [LoginController::class, 'index'])->name('login');//-> rout
 Route::post('/login', [LoginController::class, 'store']);
 
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+
+Route::get('/posts', [PostController::class, 'index'])-> name('posts');
+Route::post('/posts', [PostController::class, 'store']);
