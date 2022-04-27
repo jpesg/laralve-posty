9d30b5ae45e9250011d2c2411857a7447f051fbd
Javier.Pesado <javier.pesado@insurancerevolution.co.uk>
2022-04-27 16:41:04 +0100

    likes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,17 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable =[
+    protected $fillable = [
         'body'
     ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function likedBy(User $user)
+    {
+        return $this->likes->contains('user_id', $user->id);
     }
 }
