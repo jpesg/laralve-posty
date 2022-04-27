18922b387a7b54440a600189abded6e7caffa915
Javier.Pesado <javier.pesado@insurancerevolution.co.uk>
2022-04-27 16:11:58 +0100

    added pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $post = Post::get();//all post --> Laravel Collection
+        $post = Post::paginate(2);
 
         return view('posts.index', [
             'posts' => $post
