e243013d42c148e033a825df594f5a05aa794780
Javier.Pesado <javier.pesado@insurancerevolution.co.uk>
2022-04-27 21:32:10 +0100

    send email for liked posts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\PostLiked;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class DashboardController extends Controller
 {
-
+//php artisan make:mail PostLiked --markdown=emails.posts.post_liked
+//php artisan make:migration add_soft_deletes_to_likes_table --table=likes
     //apply  middleware inside controller
     public function __construct()
     {
